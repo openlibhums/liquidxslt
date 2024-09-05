50d6a2942543b0d1e12257f11c54f1fe275b749f
Andy Byers <a.byers@bbk.ac.uk>
2024-09-05 16:35:35 +0100

    Updates plugin for 1.7 release.

--- urls.py
+++ urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from plugins.liquidxslt import views
 
 
 urlpatterns = [
-    url(r'^manager/$', views.manager, name='liquidxslt_manager'),
-    url(r'^edit/(?P<xsl_file_id>\d+)/$', views.edit_xslt_file, name='liquidxslt_edit_xslt_file'),
+    re_path(r'^manager/$', views.manager, name='liquidxslt_manager'),
+    re_path(r'^edit/(?P<xsl_file_id>\d+)/$', views.edit_xslt_file, name='liquidxslt_edit_xslt_file'),
 ]
